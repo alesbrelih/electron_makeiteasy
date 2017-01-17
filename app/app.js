db1b8163786b9ebbaa727834c64d8ec0a69e8f35
alesbrelih <ales.brelih@gmail.com>
2017-01-17 23:14:52 +0100

    Added tooltips.
    Tooltips for indexpage component. For icons on the top right. So its a bit clearer what they represent.

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ const angular = require("angular");
 
 
 //create app
-const app = angular.module("mainWrapper",[require("angular-ui-router")]);
+const app = angular.module("mainWrapper",[require("angular-ui-router"),require("angular-animate"),require("angular-ui-bootstrap")]);
 
 //configure app
 require("./config/app.config")(app);
