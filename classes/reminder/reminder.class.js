48a64b3df87b35fa36700038d3cff19406a94ace
alesbrelih <ales.brelih@gmail.com>
2017-01-24 19:46:53 +0100

    Updated reminder class.
    Now its constructor just takes minutes and hours.
    
    Days used is set using 2 methods. 1 method takes array of days as parameter. Second one takes string.
    
    Updated unit tests for reminder class.

--- classes/reminder/reminder.class.js
+++ classes/reminder/reminder.class.js
@@ -3,15 +3,12 @@
 
 class Reminder{
 
-    constructor(minutes,hour,daysofweek){
+    constructor(minutes,hour){
         //minute - int (0-59)
         //hour - int (0-23)
-        //dayofweek - string - cron specific
 
         this.Hour = hour;
         this.Minutes = minutes;
-        this.Days = daysofweek;
-
 
     }
 
@@ -25,6 +22,9 @@ class Reminder{
     }
     get Days(){
         return this.daysofweek;
+    }
+    get DaysArray(){
+        return this.daysofweekArray;
     }
 
     // --- setters --- //
@@ -47,9 +47,46 @@ class Reminder{
         }
 
     }
-    set Days(daysofweek_){
-        if(daysofweek_.match(/.[[0-6]|[,-]]/)){
-            this.daysofweek = daysofweek_;
+
+    //set days from array
+    setDaysFromArray(daysArray_){
+        //transform array to string value aswell (for db value)
+        let daysString = "";
+        for(let dayInt of daysArray_){
+            //check for invalid int
+            if(dayInt<0 || dayInt>6){
+                throw new Error("Day integer isnt valid!");
+            }
+            else{
+                if(daysString.trim().length === 0){
+                    daysString += dayInt.toString();
+                }
+                else{
+                    daysString += `, ${dayInt.toString()}`;
+                }
+            }
+
+        }
+
+        //save both
+
+        //save string
+        this.daysofweek = daysString;
+
+        //save array
+        this.daysofweekArray = daysArray_;
+    }
+
+    //set days from string
+    setDaysFromString(daysStr_){
+
+        //check if there are any invalid chars in the string
+        const invalidchars = daysStr_.match(/[^0-6,]/g);
+
+        if(invalidchars === null){
+            this.daysofweek = daysStr_;
+            //TODO: IMPLEMENT FOR - IF NEEDED
+            this.daysofweekArray = daysStr_.trim().split(",");
         }
         else{
             throw new Error("Days of week is not in correct format");
--- tests/classes/reminder_class/reminder.class.js
+++ tests/classes/reminder_class/reminder.class.js
@@ -12,34 +12,41 @@ function testingReminderClass(chai){
 
         it("Should not throw any errors when valid values",function(){
 
-            const reminderObj = new Reminder(50,12,"1,2,3,4");
+            const reminderObj = new Reminder(50,12);
+            reminderObj.setDaysFromString("1,2,3,4");
 
             expect(reminderObj).to.not.equal(null);
+            expect(reminderObj.DaysArray.length).to.equal(4);
 
         });
         it("Should not throw any errors when valid values - second test",function(){
 
-            const reminderObj = new Reminder(50,12,"0-6");
+            const reminderObj = new Reminder(50,12);
+            reminderObj.setDaysFromString("1");
 
             expect(reminderObj).to.not.equal(null);
+
 
         });
 
         it("Should throw err if invalid input",function(){
+            const reminderObj = new Reminder(50,11);
             expect(()=>{
-                const reminderObj = new Reminder(50,11,"baba");
+                reminderObj.setDaysFromString("baba");
             }).to.throw("Days of week is not in correct format");
 
         })
         it("Should throw err if invalid input - second test",function(){
+            const reminderObj = new Reminder(50,11);
             expect(()=>{
-                const reminderObj = new Reminder(50,11,"1-9");
+                reminderObj.setDaysFromString("1-9");
             }).to.throw("Days of week is not in correct format");
 
         })
         it("Should throw err if invalid input - third test",function(){
+            const reminderObj = new Reminder(50,11);
             expect(()=>{
-                const reminderObj = new Reminder(50,11,"1:9");
+                reminderObj.setDaysFromString("1:9");
             }).to.throw("Days of week is not in correct format");
 
         })
